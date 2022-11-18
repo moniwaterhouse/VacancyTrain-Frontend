21fca44780feb82c97a7dd24317522942ce7994f
Moni Waterhouse <monica.waterhouse@estudiantec.cr>
2022-11-18 09:08:06 -0600

    Configure target url to make connection with server

--- vacancy-train/src/environments/environment.ts
+++ vacancy-train/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://10.42.0.88:5000/'
 };
 
 /*
